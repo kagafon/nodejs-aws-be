f852b477a3001334e94ce651c5029a4bff79733f
kagafon <kagafon@users.noreply.github.com>
2020-11-06 23:34:05 +0300

    init: add DB init script

---
+++ product-service/db/db_init.sql
@@ -0,0 +1,25 @@
+CREATE TABLE public.products (
+	id uuid NOT NULL,
+	title text NOT NULL,
+	description text NULL,
+	price numeric NULL,
+	CONSTRAINT products_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE public.stocks (
+	count integer NOT NULL,
+	product_id uuid NOT NULL,
+	CONSTRAINT stocks_fk FOREIGN KEY (product_id) REFERENCES public.products(id) ON DELETE CASCADE
+);
+
+insert into products (id, title, description, price) values (gen_random_uuid(), 'Square', 'Most equal square', 25.5);
+insert into products (id, title, description, price) values (gen_random_uuid(), 'Circle', 'Best rounded shape', 13.4);
+insert into products (id, title, description, price) values (gen_random_uuid(), 'Octagon', 'Fighting shape', 25.5);
+insert into products (id, title, description, price) values (gen_random_uuid(), 'Triangle', 'Only three corners', 177.1);
+insert into products (id, title, description, price) values (gen_random_uuid(), 'Rhombus', 'Skew this square!', 5.3);
+insert into products (id, title, description, price) values (gen_random_uuid(), 'Trapezoid', 'Just another quadrangle', 4.1);
+insert into products (id, title, description, price) values (gen_random_uuid(), 'Line segment', 'Base of everything', 72.2);
+insert into products (id, title, description, price) values (gen_random_uuid(), 'Hexagon', 'Organic сhemistry, you know', 98.9);
+
+insert into stocks (product_id, count) select id, trunc(random()*40) from products;
+commit;
