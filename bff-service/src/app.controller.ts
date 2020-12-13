49b35b95dab034aba08b3ea92a43ee7a440c0969
kagafon <kagafon@users.noreply.github.com>
2020-12-13 20:06:34 +0300

    fix: restore query params passing, add headers

--- bff-service/src/app.controller.ts
+++ bff-service/src/app.controller.ts
@@ -21,13 +21,13 @@ export class AppController {
   @All()
   async proxyAll(@Req() req: Request, @Res() res: Response) {
     const pathParts = req.url.split('?')[0].split('/');
-    const sourceUrl = `${process.env[pathParts[1]]}/${pathParts
-      .slice(2)
-      .join('/')}`;
+    const sourceUrl = [process.env[pathParts[1]], ...pathParts.slice(2)].join(
+      '/',
+    );
 
     if (pathParts[1] && process.env[pathParts[1]]) {
       return this.proxyService
-        .getResponse(req.method, sourceUrl, req.body)
+        .getResponse(req.method, sourceUrl, req.body, req.query, req.headers)
         .then((resp) => {
           if (req.method === 'GET' && this.cachedPaths.includes(req.path)) {
             this.cacheService.setCachedResponse(req.path, resp);
--- bff-service/src/services/proxy.service.ts
+++ bff-service/src/services/proxy.service.ts
@@ -7,16 +7,20 @@ export class ProxyService {
     method: string,
     url: string,
     data: Record<string, unknown>,
+    params: Record<string, unknown>,
+    headers: Record<string, unknown>,
   ): Promise<AxiosResponse<unknown>> {
     // API Gateway returns 403 if GET request has a body.
     // Axios.request() always send a body
     if (method === 'GET') {
-      return axios.get(url);
+      return axios.get(url, { params, headers });
     }
     return axios.request<Record<string, unknown>>({
       method: method as Method,
       url,
       ...(Object.keys(data || {}).length > 0 ? data : {}),
+      params,
+      headers,
     });
   }
 }
